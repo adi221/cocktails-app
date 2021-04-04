6ee195d18a669224d00f4acef4b483c0ae7d8a7d
adi221 <adimizrahi421@gmail.com>
2021-04-04 09:33:42 +0300

    Added Routing and initial pattern

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { SingleDrink, Drinks, Home, Ingredients } from './pages';
+import { ScrollToTop } from './components';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Router>
+      <ScrollToTop />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/drinks'>
+          <Drinks />
+        </Route>
+        <Route exact path='/drink/:id' children={<SingleDrink />} />
+        <Route exact path='/ingredients/:name' children={<Ingredients />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
